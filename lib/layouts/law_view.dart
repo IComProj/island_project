3cb47415f742195617480627b599ba9d555067c4
IComProj <islandcommunityproject@gmail.com>
2022-12-04 14:41:57 +0100

    Added: Law view

--- lib/layouts/law_view.dart
+++ lib/layouts/law_view.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:island_project/layouts/primitive_layouts.dart';
 
+class LawView extends StatefulWidget {
+  const LawView({super.key});
+
+  @override
+  State<LawView> createState() => _LawViewState();
+}
+
+class _LawViewState extends State<LawView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(floatingActionButton: const ReturnFloatingActionButton());
+  }
+}
