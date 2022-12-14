e26feb9f90f24f534db0626d3e06484a0a1ca599
IComProj <islandcommunityproject@gmail.com>
2022-12-14 19:52:45 +0100

    Modified: actions

--- lib/data/jobs/actions.dart
+++ lib/data/jobs/actions.dart
@@ -1,5 +1,54 @@
 part of 'jobs.dart';
 
+class JobAction {
+  const JobAction(this.actionName,
+      {this.displayName,
+      this.onActivate,
+      this.checkAllowance,
+      this.iconData,
+      this.requirements,
+      this.requirementIndicator});
+  final String actionName;
+  final IconData? iconData;
+  final String Function()? displayName;
+  final Function(UserData, Things)? onActivate;
+  final bool Function(UserData, Things)? checkAllowance;
+  final Map<String, int>? requirements;
+  final Function<bool>()? requirementIndicator;
+
+  void activate(UserData user, Things things) {
+    if (onActivate != null) {
+      onActivate!(user, things);
+    }
+  }
+
+  bool isAllowed(UserData user, Things things) {
+    if (checkAllowance == null) return true;
+
+    return checkAllowance!(user, things);
+  }
+
+  // bool isActivateable(Things things) {
+  //   if (requirements == null) return true;
+
+  //   for (var requiredResource in requirements!.entries) {
+  //     String resourceName = requiredResource.key;
+
+  //     if (!things.content.containsKey(resourceName)) return false;
+
+  //     if (things.content[resourceName]! < requiredResource.value) return false;
+  //   }
+
+  //   //we are through the loop: means we can de custom validation
+
+  //   if (requirementIndicator != null) {
+  //     return requirementIndicator!();
+  //   }
+
+  //   return true;
+  // }
+}
+
 ///A list of job actions
 class JobActionsList {
   JobActionsList();
@@ -15,39 +64,44 @@ class JobActionsList {
   }
 }
 
-///A collection of job actions
-class ActionCollection {
-  ActionCollection._();
-
-  static ActionCollection instance = ActionCollection._();
-
-  JobAction get buildHouseAction => all[0]!;
-  JobAction get huntAction => all[1]!;
-  JobAction get placeTrapAction => all[2]!;
-
-  //TODO: Solve update anomaly
-  final Map<int, JobAction> all = {
-    0: _buildHouseAction,
-    1: _huntAction,
-    2: _placeTrapAction
-  };
-}
-
 final _buildHouseAction = JobAction(
-  "Haus bauen",
-  onActivate: () {
+  "act_build_house",
+  onActivate: (user, things) {
     FirebaseUtilities.instance.setLastActivation();
   },
   requirements: {},
   iconData: UniconsLine.shovel,
 );
 
-final _huntAction = JobAction("Jagen", onActivate: () {
+final _huntAction = JobAction("act_hunt", onActivate: (user, things) {
   FirebaseUtilities.instance.setLastActivation();
 }, iconData: Ionicons.locate, requirements: {});
 
-final _placeTrapAction = JobAction("Place Trap", onActivate: () {
+final _placeTrapAction =
+    JobAction("act_place_trap", onActivate: (user, things) {
   FirebaseUtilities.instance.setLastActivation();
 }, iconData: Ionicons.locate, requirements: {
   ResourceName.shovel: 1,
 });
+
+///A collection of job actions
+class ActionCollection {
+  ActionCollection._();
+
+  static ActionCollection instance = ActionCollection._();
+
+  //to register an new action:
+
+  //insert the created action instance
+  //HERE:
+  JobAction get buildHouseAction => _buildHouseAction;
+  JobAction get huntAction => _huntAction;
+  JobAction get placeTrapAction => _placeTrapAction;
+
+  //AND HERE:
+  final List<JobAction> all = [
+    _buildHouseAction,
+    _huntAction,
+    _placeTrapAction
+  ];
+}
