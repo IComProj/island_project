67955c16e7f2ffb94f803ceff6f8b9c4109cf75f
IComProj <islandcommunityproject@gmail.com>
2022-12-28 22:47:12 +0100

    Modified: Removed a bit of spaghetti code

--- lib/layouts/home_view.dart
+++ lib/layouts/home_view.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:island_project/data/laws/laws.dart';
 import 'package:island_project/data/notification.dart' as notification;
 import 'package:island_project/data/thing.dart';
 import 'package:island_project/data/userdata.dart';
@@ -9,6 +10,7 @@ import 'package:island_project/layouts/primitive_layouts.dart';
 import 'package:island_project/layouts/sign_in_page.dart';
 import 'package:island_project/utilities/firebase_utilities.dart';
 import 'package:island_project/data/jobs/jobs.dart';
+import 'package:island_project/utilities/law_utility.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -17,12 +19,128 @@ class HomeView extends StatefulWidget {
   State<StatefulWidget> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
-  bool isSchedulingJob = false;
+class ChooseJobMenu extends StatefulWidget {
+  const ChooseJobMenu({super.key, required this.userData, this.onChooseJob});
+
+  final UserData userData;
+  final Function()? onChooseJob;
+
+  @override
+  State<ChooseJobMenu> createState() => _ChooseJobMenuState();
+}
+
+class _ChooseJobMenuState extends State<ChooseJobMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        NotificationCard(
+            notification: notification.Notification(
+                "",
+                "Das ist dein |Zuhause|. Hier arbeitest du (in diesem Spiel gibt's nur HomeOffice...).",
+                0)),
+        const SizedBox(
+          height: 20,
+        ),
+        NotificationCard(
+            notification: notification.Notification(
+                "",
+                "Als erstes musst du deinen |Beruf| wählen. Clicke dich dazu einfach durch den Dropdown (unten).[ENTER]Und keine Panik! Du kannst den Beruf später noch ändern.",
+                0),
+            align: TextAlign.center),
+        _buildJobDropdown(),
+        JobDescription(job: Jobs.all[_dropdownValue ?? 0]),
+        TextButton(
+            style: Theme.of(context).textButtonTheme.style,
+            onPressed: () {
+              var userData = widget.userData;
+
+              userData.job = Jobs.all[_dropdownValue ?? 0].jobName;
+
+              FirebaseUtilities.instance.updateUserData(userData);
+
+              widget.onChooseJob?.call();
+            },
+            child: Text(
+              style: Theme.of(context).textTheme.button,
+              "Job wählen",
+            ))
+      ],
+    );
+  }
+
+  int? _dropdownValue = 0;
+
+  Widget _buildJobDropdown() {
+    //remove all jobs that are currently not allowed
+    var jobs = Jobs.all
+        .where((element) => !LawStack.instance.test(element.jobName))
+        .toList();
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        DropdownButton<int>(
+          value: _dropdownValue,
+          items: List.generate(jobs.length, (index) {
+            return DropdownMenuItem(
+                value: index,
+                child: Row(
+                  children: [
+                    Icon(
+                      jobs[index].iconData,
+                    ),
+                    Text(
+                      "   ${jobs[index].displayName}",
+                      style: Theme.of(context).textTheme.headline5,
+                    )
+                  ],
+                ));
+          }),
+          onChanged: (value) {
+            setState(() {
+              _dropdownValue = value;
+            });
+          },
+        ),
+        const SizedBox(
+          height: 20,
+        )
+      ],
+    );
+  }
+}
+
+class JobDescription extends StatelessWidget {
+  const JobDescription({required this.job, super.key});
+
+  final Job job;
 
   @override
   Widget build(BuildContext context) {
-    var currentUserLoader = FirebaseUtilities.instance.getCurrentUserData();
+    return Padding(
+      padding: const EdgeInsets.all(0),
+      child: NotificationCard(
+          notification: notification.Notification("", job.description, 0),
+          align: TextAlign.center),
+    );
+  }
+}
+
+class _HomeViewState extends State<HomeView> {
+  bool _isSchedulingJob = false;
+
+  @override
+  Widget build(BuildContext context) {
+    //only show something if the userdata is loaded AND the laws are synced
+    var currentUserLoader =
+        FirebaseUtilities.instance.getCurrentUserData().then((value) async {
+      await LawStack.pull();
+      return value;
+    });
 
     return FutureBuilder(
       builder: (context, userSnapshot) {
@@ -32,40 +150,9 @@ class _HomeViewState extends State<HomeView> {
           if (userSnapshot.data!.job.isEmpty) {
             //if the user hasn't choosen his job yet, build the menu for doing so:
 
-            body = Column(
-              children: [
-                NotificationCard(
-                    notification: notification.Notification(
-                        "",
-                        "Das ist dein |Zuhause|. Hier arbeitest du (in diesem Spiel gibt's nur HomeOffice...).",
-                        0)),
-                const SizedBox(
-                  height: 20,
-                ),
-                NotificationCard(
-                    notification: notification.Notification(
-                        "",
-                        "Als erstes musst du deinen |Beruf| wählen. Clicke dich dazu einfach durch den Dropdown (unten).[ENTER]Und keine Panik! Du kannst den Beruf später noch ändern.",
-                        0),
-                    align: TextAlign.center),
-                buildJobDropdown(),
-                buildJobDescription(Jobs.all[_dropdownValue ?? 0]),
-                TextButton(
-                    style: Theme.of(context).textButtonTheme.style,
-                    onPressed: () {
-                      var userData = userSnapshot.data!;
-
-                      userData.job = Jobs.all[_dropdownValue ?? 0].displayName;
-
-                      FirebaseUtilities.instance.updateUserData(userData);
-
-                      setState(() {});
-                    },
-                    child: Text(
-                      style: Theme.of(context).textTheme.button,
-                      "Job wählen",
-                    ))
-              ],
+            body = ChooseJobMenu(
+              userData: userSnapshot.data!,
+              onChooseJob: () => setState(() {}),
             );
           } else {
             //if the user has already choosen his job, draw the actions he can execute, his current ressources, etc...
@@ -74,6 +161,22 @@ class _HomeViewState extends State<HomeView> {
             UserData currentUser = userSnapshot.data!;
 
             if (job == null) return const ErrorPage();
+
+            ///Change job if there's a trigger
+            if (LawStack.instance.test(job.jobName)) {
+              currentUser.job = "";
+
+              FirebaseUtilities.instance.updateUserData(currentUser);
+
+              return Scaffold(
+                body: ChooseJobMenu(
+                  userData: currentUser,
+                  onChooseJob: () => setState(() {}),
+                ),
+                floatingActionButton: const ReturnFloatingActionButton(
+                    arrowDirection: AxisDirection.right),
+              );
+            }
 
             var thingsLoader = FirebaseDatabase.instance
                 .ref("things/${userSnapshot.data!.uid}")
@@ -98,7 +201,7 @@ class _HomeViewState extends State<HomeView> {
                     Text(job.displayName,
                         style: Theme.of(context).textTheme.displaySmall)
                   ]),
-                  buildJobDescription(job),
+                  JobDescription(job: job),
                   Text(
                     "Aktionen",
                     style: Theme.of(context).textTheme.headline4,
@@ -111,7 +214,7 @@ class _HomeViewState extends State<HomeView> {
                           0))
                 ]);
 
-                menu.addAll(buildActionsMenu(
+                menu.addAll(_buildActionsMenu(
                     job, things.data ?? Things.empty(), currentUser));
 
                 return ListView(children: menu);
@@ -133,55 +236,7 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 
-  int? _dropdownValue = 0;
-
-  Widget buildJobDropdown() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const SizedBox(
-          height: 20,
-        ),
-        DropdownButton<int>(
-          value: _dropdownValue,
-          items: List.generate(Jobs.all.length, (index) {
-            return DropdownMenuItem(
-                value: index,
-                child: Row(
-                  children: [
-                    Icon(
-                      Jobs.all[index].iconData,
-                    ),
-                    Text(
-                      "   ${Jobs.all[index].displayName}",
-                      style: Theme.of(context).textTheme.headline5,
-                    )
-                  ],
-                ));
-          }),
-          onChanged: (value) {
-            setState(() {
-              _dropdownValue = value;
-            });
-          },
-        ),
-        const SizedBox(
-          height: 20,
-        )
-      ],
-    );
-  }
-
-  Widget buildJobDescription(Job job) {
-    return Padding(
-      padding: const EdgeInsets.all(0),
-      child: NotificationCard(
-          notification: notification.Notification("", job.description, 0),
-          align: TextAlign.center),
-    );
-  }
-
-  List<Widget> buildActionsMenu(Job job, Things things, UserData currentUser) {
+  List<Widget> _buildActionsMenu(Job job, Things things, UserData currentUser) {
     return job.getActions().actions.map((a) {
       DateTime lastActivation = currentUser.lastActivation == ""
           ? DateTime(0)
@@ -199,7 +254,7 @@ class _HomeViewState extends State<HomeView> {
       int diff = lastActivation.difference(now).inDays.abs();
 
       bool isActivateable =
-          a.isAllowed(currentUser, things) && !isSchedulingJob && diff >= 1;
+          a.isAllowed(currentUser, things) && !_isSchedulingJob && diff >= 1;
 
       debugPrint(
           "Last activation ${lastActivation.difference(DateTime.now()).inDays.abs()} ago.");
@@ -213,13 +268,13 @@ class _HomeViewState extends State<HomeView> {
             children: [
               Text(a.displayName(),
                   style: Theme.of(context).textTheme.headline4),
-              diff >= 1 && !isSchedulingJob
+              diff >= 1 && !_isSchedulingJob
                   ? TextButton(
                       onPressed: isActivateable
                           ? () {
                               a.activate(currentUser, things);
                               setState(() {
-                                isSchedulingJob = true;
+                                _isSchedulingJob = true;
                               });
                             }
                           : null,
